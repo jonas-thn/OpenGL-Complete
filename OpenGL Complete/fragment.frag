10034e1a2198c1558722d7092ad24c8839093030
Jonas Thaun <thaunjonas@gmail.com>
2025-01-08 23:00:56 +0100

    textures working

--- OpenGL Complete/fragment.frag
+++ OpenGL Complete/fragment.frag
@@ -1,8 +1,12 @@
 #version 330 core
 
+in vec2 texcoord;
+
 out vec4 FragColor;
+
+uniform sampler2D texture0;
 
 void main()
 {
-	FragColor = vec4(0.0, 0.5, 0.8, 1.0);
+	FragColor = texture(texture0, texcoord);
 }
--- OpenGL Complete/main.cpp
+++ OpenGL Complete/main.cpp
@@ -7,6 +7,9 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #include "Shader.h"
 
 const int WIDTH = 800;
@@ -17,10 +20,10 @@ SDL_GLContext glContext;
 bool close = false;
 
 float vertices[] = {
-0.5f, 0.5f, 0.0f, // top right
-0.5f, -0.5f, 0.0f, // bottom right
--0.5f, -0.5f, 0.0f, // bottom left
--0.5f, 0.5f, 0.0f // top left
+0.5f, 0.5f, 0.0f, 1.0f, 1.0f, // top right
+0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+-0.5f, 0.5f, 0.0f, 0.0f, 1.0f // top left
 };
 
 unsigned int indices[] = { 
@@ -33,6 +36,8 @@ unsigned int VAO;
 unsigned int EBO;
 
 std::unique_ptr<Shader> shader = nullptr;
+
+unsigned int texture;
 
 void init()
 {
@@ -69,14 +74,20 @@ void init()
 
 void setup()
 {
+	//----------VBO, VAO, EBO----------
+
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
 
 		glGenBuffers(1, &EBO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -84,7 +95,45 @@ void setup()
 
 	glBindVertexArray(0);
 
+	//----------SHADER----------
+
 	shader = std::make_unique<Shader>("vertex.vert", "fragment.frag");
+
+	//----------TEXTURE----------
+
+	stbi_set_flip_vertically_on_load(true);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("Textures/container.jpg", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		if (nrChannels == 3)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else if (nrChannels == 4)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+	}
+	else
+	{
+		std::cout << "Texture ERROR" << std::endl;
+	}
+
+	stbi_image_free(data);
 }
 
 void process_input()
@@ -109,8 +158,11 @@ void render()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	shader->UseShader();
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	shader->SetInt("texture0", 0);
 	glBindVertexArray(VAO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 	SDL_GL_SwapWindow(window);
@@ -130,6 +182,7 @@ int main(int argc, char* argv[])
 
 	while (!close)
 	{
+		process_input();
 		update();
 		render();
 	}
